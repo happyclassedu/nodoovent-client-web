451cd498e6b3e2e47a0a750eb9e5bb57319a06e0
Simon RENOULT <g4llic4@gmail.com>
2012-05-11 19:44:37 +0700

    Update comments

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-
-/**
+/*
  * Module dependencies.
+ *  express is included within the node_modules folder.
+ *  routes is a module coming from the routes folder.
  */
-
 var express = require('express')
   , routes = require('./routes');
 
+/*
+ * Create an express server instance.
+ */
 var app = module.exports = express.createServer();
 
-// Configuration
-
+/*
+ * Configuration.
+ * Define the view engine (jade) and some other stuff. Check documentation.
+ */
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -23,10 +28,15 @@ app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-// Routes
-
+/*
+ * Route the required path.
+ */
 app.get('/', routes.index);
 
+/*
+ * Bind a port on the server instance. Once the port is bound, the function
+ * executes a callback displaying informations on the terminal.
+ */
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
